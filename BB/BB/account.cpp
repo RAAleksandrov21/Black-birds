ff43c9f2c2c7844670bea5280c3fee07852959db
EEMustafa21 <EEMustafa21@codingburgas.bg>
2024-01-15 04:47:32 +0200

    Add withdraw

--- BB/BB/account.cpp
+++ BB/BB/account.cpp
@@ -2,3 +2,13 @@ void Account::deposit(double amount) {
     balance += amount;
     std::cout << "Deposit successful. New balance: " << balance << std::endl;
 }
+
+void Account::withdraw(double amount) {
+    if (balance >= amount) {
+        balance -= amount;
+        std::cout << "Withdrawal successful. New balance: " << balance << std::endl;
+    }
+    else {
+        std::cout << "Insufficient funds.\n";
+    }
+}
